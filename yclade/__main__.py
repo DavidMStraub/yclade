9fc1c74787fc0eb2539a82f503d3240fad57f0d1
David Straub <straub@protonmail.com>
2025-02-01 15:08:32 +0100

    Update CLI, Readme

--- yclade/__main__.py
+++ yclade/__main__.py
@@ -6,13 +6,18 @@ import yclade
 
 
 @click.command()
-@click.argument("snp_string")
+@click.argument("snp_string", required=False)
 @click.option("--version", default=None, help="The YFull version to use (optional).")
 @click.option(
     "--data-dir", default=None, help="The directory to store the YFull data (optional)."
 )
-def main(snp_string, version, data_dir):
+@click.option("--file", "-f", type=click.File("r"))
+def main(snp_string, version, data_dir, file):
     """Find the clade that matches the given SNP string."""
+    if not snp_string and not file:
+        raise click.UsageError("You must provide either a SNP string or a file.")
+    if file:
+        snp_string = file.read().strip()
     clade_info = yclade.find_clade(snp_string, version=version, data_dir=data_dir)
     for info in clade_info:
         click.echo(info)
